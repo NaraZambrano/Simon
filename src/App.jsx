ed2fe9925451ce57f84be8777b1811b4347ce94d
NaraZambrano <lourdes.zambrano@ikasle.aeg.eus>
2024-03-14 12:28:50 +0100

    inicial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+   /* crear un useState*/
+  const [Colors, setColor] = useState(['#bd0b51', '#093679', '#32ac17', '#d0e92a']);
+  const[sequence, setSequence]= useState([]);
+  const handleClick = (index) =>
+{
+  console,console.log((index));
+}
+const randomNumber = () => {
+  const minNumber = 0;
+  const maxNumber = 3;
+  /*coger un random entre dos valores mirar cofigo en documentacion que sean inclusivos mochiladeveloper mwd*/
+  const randomNumber = Math.random() * (maxNumber - minNumber +1) + minNumber;
+  setSequence([...sequence, randomNumber]);/*sequencia hecha con gia de mocilla*/
+}
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='header'>
+        <h1>Super Simon</h1>
+        <p>(sequence)</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='container'>
+        {
+          Colors.map((color, index) => {
+        return <div 
+        key={index} 
+        className='pad pad-${index}' 
+        style={{
+          backgroundColor:`$(color)`}}
+        onClock={() => handleClick(index)}
+        >
+        </div>
+
+      })
+
+      }
+    </div>
+      
+
     </>
   )
 }
 
 export default App
+/*
+1. hacer comentarios 
+2. 
+7. validar 
+8. si acepta la validacion  (solved)
+*/
