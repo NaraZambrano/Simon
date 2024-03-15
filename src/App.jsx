a32f50eb5181cbca92de9951e69dd86cca5462af
NaraZambrano <lourdes.zambrano@ikasle.aeg.eus>
2024-03-15 10:51:51 +0100

    cambiosç

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,122 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
+
 function App() {
-   /* crear un useState*/
-  const [Colors, setColor] = useState(['#bd0b51', '#093679', '#32ac17', '#d0e92a']);
-  const[sequence, setSequence]= useState([]);
-  const handleClick = (index) =>
-{
-  console,console.log((index));
-}
-const randomNumber = () => {
-  const minNumber = 0;
-  const maxNumber = 3;
-  /*coger un random entre dos valores mirar cofigo en documentacion que sean inclusivos mochiladeveloper mwd*/
-  const randomNumber = Math.random() * (maxNumber - minNumber +1) + minNumber;
-  setSequence([...sequence, randomNumber]);/*sequencia hecha con gia de mocilla*/
-}
-  return (
-    <>
-      <div className='header'>
-        <h1>Super Simon</h1>
-        <p>(sequence)</p>
-      </div>
-      <div className='container'>
-        {
-          Colors.map((color, index) => {
-        return <div 
-        key={index} 
-        className='pad pad-${index}' 
-        style={{
-          backgroundColor:`$(color)`}}
-        onClock={() => handleClick(index)}
-        >
-        </div>
 
-      })
 
-      }
-    </div>
-      
+ const [colors, setColor] = useState([
+   {
+     color:'#030AFA',
+     opacity: 0.2
+   },
+   {
+     color:'#FAF303',
+     opacity: 0.2
+   },
+   {
+     color:'#0AFA03',
+     opacity: 0.2
+   },
+   {
+     color:'#FA0E03',
+     opacity: 0.2
+   }
+ ]);
+ const [sequence, setSequence] = useState([]);
+ const [curretnGame, setCurrentGame] = useState([]);
+ const [turn, setTurn] = useState (null);
 
-    </>
-  )
+ const handleClick = (index) => {
+  setCurrentGame([...curretnGame, index])
+  //check if index === sequence[turn]
+
+  
+  
+ }
+
+
+ const randomNumber = () => {
+   const minNumber = 0;
+   const maxNumber = 3;
+   const randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
+   setSequence([...sequence, randomNumber]);
+ }
+
+ useEffect(() => {
+   console.log(colors.length)
+   randomNumber();
+    return () => {
+     setSequence([]);
+   }
+ }, [])
+
+  useEffect(() => {
+   const tempArray = [...colors];
+  
+   sequence.map(index => {
+     tempArray[index].opacity = 1;
+     setColor(tempArray);
+     setTimeout(() => {
+       console.log("Retrasado por 1 segundo.");
+       tempArray[index].opacity = 0.2;
+       setColor(tempArray);
+     }, 1000);
+   }) 
+ }, [sequence])
+
+
+ return (
+   <>
+     <div className='header'>
+       <h1>SUPER SIMON</h1>
+       <p>Game{sequence}</p>
+       <p>Payer{setCurrentGame}</p>
+     </div>
+     <div className='container'>
+       {
+        
+         colors.map((item, index) => {
+           return (<div
+             key={index}
+             className={`pad pad-${index}`}
+             style={{backgroundColor:`${item.color}`, opacity:`${item.opacity}`}}
+             onClick={() => handleClick(index)}
+           >
+           </div>)
+         })
+       } 
+     </div> 
+    
+    
+   </>
+ )
 }
+
 
 export default App
+
+
 /*
-1. hacer comentarios 
-2. 
-7. validar 
-8. si acepta la validacion  (solved)
+1.- Interfaz de cartón[SOLVED]
+2.- Detectar click en los componentes[SOLVED]
+3.- Crear array para almacenar la secuencia del juego[SOLVED]
+4.- Crear el color random para añadirlo a la secuencia [SOLVED]
+5.- Reproducir la secuencia -- HARD
+   a)Iniciar color, activar color
+   b)Establecer un timeout, activamos y desactivamos colores
+   c)Recorrer secuencia
+   f)Al finalizar la secuencia debemos saber que cambia el turno al jugador
+6.- Usuario intenta reproducir secuencia
+7.- Validar secuencia usuario por cada pulsación
+8.- Si acierta la pulsación sigue el juego
+9.- Si no acierta el juego termina ylimpiamos la secuencia del juego
+
+
+OPCIONAL
+-Máxima puntuación
+-Sonido
+-Tiempo
+
+
 */
